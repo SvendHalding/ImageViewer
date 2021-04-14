5c4b35da524b0ea34cbf62c086af003be9b7c1ee
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2021-04-14 10:16:17 +0200

    push to the limit
    push to the limit

--- src/dk/easv/ImageViewerWindowController.java
+++ src/dk/easv/ImageViewerWindowController.java
@@ -3,9 +3,15 @@ package dk.easv;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.File;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
@@ -15,13 +21,59 @@ import javafx.stage.Stage;
 public class ImageViewerWindowController
 {
     private final List<Image> images = new ArrayList<>();
+    public Button btnStartStopSlide;
+    public Slider slider;
+    public Label imageName;
     private int currentImageIndex = 0;
+    private int sliderTimer = 5000;
+    private boolean sliderRunning = false;
 
     @FXML
     Parent root;
 
     @FXML
     private ImageView imageView;
+
+    public void initialize() {
+    slider.valueProperty().addListener((observable, oldvalue, newvalue)-> {sliderTimer = newvalue.intValue();} );
+    }
+
+
+    @FXML
+    private  void handleBtnStartStop() {
+
+        if (!sliderRunning) {
+            sliderRunning = true;
+            btnStartStopSlide.setText("Stop Slider");
+
+            Task task = new Task<Void>() {
+                @Override
+                public Void call() throws Exception {
+                while(sliderRunning) {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            displayImage();
+                            currentImageIndex = (currentImageIndex + 1) % images.size();
+                        }
+                    });
+
+                    Thread.sleep(sliderTimer);
+                }
+                return null;
+                }
+
+            };
+            Thread th = new Thread(task);
+            th.setDaemon(true);
+            th.start();
+        }
+        else {
+            sliderRunning = false;
+            btnStartStopSlide.setText("Start Slider");
+        }
+
+    }
 
     @FXML
     private void handleBtnLoadAction()
@@ -67,7 +119,13 @@ public class ImageViewerWindowController
     {
         if (!images.isEmpty())
         {
-            imageView.setImage(images.get(currentImageIndex));
+            Image image = images.get(currentImageIndex);
+            imageView.setImage(image);
+            String url = image.getUrl();
+            String[] parts = url.split("/");
+            String fileName = parts[parts.length - 1];
+            imageName.setText(fileName);
+
         }
     }
 }
